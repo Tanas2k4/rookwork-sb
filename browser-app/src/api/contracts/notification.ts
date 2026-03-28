1b13597ecc830f39b6d423965c418463d6c26a79
Tanas2k4 <bongbo355@gmail.com>
2026-03-28 13:21:30 +0700

    refactor: add avt to notifi

--- browser-app/src/api/contracts/notification.ts
+++ browser-app/src/api/contracts/notification.ts
@@ -1,10 +1,17 @@
+export interface NotificationSender {
+  id: string;
+  profileName: string;
+  picture: string | null;
+}
+
 export interface NotificationResponse {
   id: string;
   title: string;
   message: string;
   issueId: string;
   issueName: string;
-  isRead: boolean; 
-  read: boolean;    
+  isRead: boolean;
+  read: boolean;
+  sender: NotificationSender | null;
   createdAt: string;
 }
